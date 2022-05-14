8e434debc0672042b031f04b1bb8bc60d85fd45e
adan222 <adanonline2@gmail.com>
2022-05-14 16:08:26 +0200

    Addition and multiplication to BigInt

--- src/crypto/crypto_test.cpp
+++ src/crypto/crypto_test.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "crypto/math/big_int.hpp"
 
 int main() {
-    std::cout << "CryptLib test!!!\n";
+    std::cout << "CryptoLib test!!!\n";
+
+    crypto::math::BigInt one("690213");
+    crypto::math::BigInt two("124122");
+
+    one * two;
 }
--- src/crypto/math/big_int.hpp
+++ src/crypto/math/big_int.hpp
@@ -1,30 +1,69 @@
 #ifndef CRYPTO_MATH_BIG_INT_HPP_
 #define CRYPTO_MATH_BIG_INT_HPP_
 
-#include <cstdint>
+#include <iostream>
+#include <ostream>
 #include <vector>
 #include <string>
+
+#include <cstddef>
+#include <cstdint>
+#include <cmath>
 
 namespace crypto {
 namespace math {
 
+constexpr static char BI_ASCI_NUM_START = 48;
+
+enum byteOrder {
+    kLittleEnding,
+    kBigEnding
+};
+
+enum numberSign {
+    kPositive,
+    kNegative
+};
+
+typedef std::vector<std::uint8_t> BigIntLiteral;
+
+void PrintLiteral(const BigIntLiteral &lit);
+
 class BigInt {
     private:
-        std::vector<std::uint8_t> literal_;
+        BigIntLiteral literal_;
 
-        void FromString(const std::string &number);
+        numberSign sign_;
+        byteOrder bOrder_;
+
+        void SwapSign();
+        BigIntLiteral SwapLiteral(const BigIntLiteral &l) const;
+
+        /*
+         * Struct conatin the bigest and smallest 
+         * literal. GetMax() compare literal of given
+         * object with literaal of local object and return
+         * this struct. It also convert "other" litaral if 
+         * it`s different then our.
+         */
+        struct MaxLiteral {
+            BigIntLiteral big_;
+            BigIntLiteral small_;
+        };
+        MaxLiteral GetMax(const BigInt &other) const;
 
     public:
         BigInt();
-        BigInt(const std::string &number);
+        BigInt(const std::string &num, byteOrder o = kLittleEnding);
+        BigInt(const std::int64_t num, byteOrder o = kLittleEnding);
         ~BigInt();
 
         // Arithmetic operators
-        BigInt operator+(const BigInt &other) const;
-        BigInt operator-(const BigInt &other) const;
-        BigInt operator*(const BigInt &other) const;
-        BigInt operator/(const BigInt &other) const;
-        BigInt operator^(const int number) const;
+        BigInt operator+(const BigInt &other);
+        BigInt operator-(const BigInt &other);
+        BigInt operator*(const BigInt &other);
+        BigInt operator/(const BigInt &other);
+        BigInt operator%(const BigInt &other);
 
         // Comparison operators
         bool operator==(const BigInt &other) const;
@@ -33,8 +72,20 @@ class BigInt {
         bool operator<=(const BigInt &other) const;
         bool operator>(const BigInt &other) const;
         bool operator>=(const BigInt &other) const;
+
+        std::string ToString() const;
+
+        std::size_t GetSize() const;
+        byteOrder GetOrder() const;
+        numberSign GetSign() const;
+        BigIntLiteral GetLiteral() const;
+
+        void SwapEnding();
+
+        friend std::ostream& operator<<(std::ostream &os, const BigInt &num);
 };
 
+std::ostream& operator<<(std::ostream& os, const BigInt& num);
 } // namespace math
 } // namespace crypto
 
