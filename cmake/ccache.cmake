22917b80ab561e1b4e7a9ddf3e1dc9872dbca153
Kamil Fugiel <kamil@fugiel.pl>
2022-10-05 17:24:05 +0200

    Base for unit tests in Catch2
    Now if you want to build tests you must
    set CRAWL_BUILD_TEST in cmake.
    Then cmake will look for Catch2 in system
    and if he couldn`t find it, he will download
    Catch2 from github.

--- cmake/ccache.cmake
+++ cmake/ccache.cmake
@@ -1,4 +1,5 @@
 # look for ccache and include it
+message(STATUS "Looking for ccache")
 find_program(CCACHE_FOUND ccache)
 if(CCACHE_FOUND)
     set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
