bea225b8dd831d53c64644e6aa397e67d06f21e2
Kamil Fugiel <kamil@fugiel.pl>
2022-10-20 21:59:12 +0200

    Add better error message to netdb_category
    Replace switch with all netdb_errc with
    gai_strerror() function which return great
    error message.

--- src/utils/error/netdb_category.cpp
+++ src/utils/error/netdb_category.cpp
@@ -7,43 +7,7 @@ const char* netdb_category::name() const noexcept {
 }
 
 std::string netdb_category::message(int ev) const {
-    // TODO: Better error message here
-    switch (static_cast<netdb_errc>(ev)) {
-        case netdb_errc::eai_addr_family:
-            return "eai_addr_family"; break;
-
-        case netdb_errc::eai_family:
-            return "eai_family"; break;
-
-        case netdb_errc::eai_again:
-            return "eai_again"; break;
-
-        case netdb_errc::eai_fail:
-            return "eai_fail"; break;
-
-        case netdb_errc::eai_system:
-            return "eai_system"; break;
-
-        case netdb_errc::eai_badflags:
-            return "eai_badflags"; break;
-
-        case netdb_errc::eai_memory:
-            return "eai_memory"; break;
-
-        case netdb_errc::eai_nodata:
-            return "eai_nodata"; break;
-
-        case netdb_errc::eai_noname:
-            return "eai_noname"; break;
-
-        case netdb_errc::eai_service:
-            return "eai_service"; break;
-
-        case netdb_errc::eai_socktype:
-            return "eai_socktype"; break;
-        default:
-            return "unknow error"; break;
-    }
+    return ::gai_strerror(ev);
 }
 
 // Here we create the only object of netdb_category
